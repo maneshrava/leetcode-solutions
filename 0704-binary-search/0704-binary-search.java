dc3f1340b5bac3b5e0ca2e706eb7adfa05a9b36a
Shravani Mane <shravanimane33@gmail.com>
2026-07-05 21:28:25 +0530

    Time: 1 ms (10.12%), Space: 48.7 MB (13.98%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,5 +1,7 @@
 class Solution {
     public int search(int[] nums, int target) {
+        if(target > nums[nums.length - 1])
+          return -1;
         for(int i =0 ; i< nums.length ; i++){
             if(nums[i] == target)
              return i; 
