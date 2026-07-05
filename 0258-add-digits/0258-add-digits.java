8fe862d7aea62882bb7ab6a63d61eb290c408b2a
Shravani Mane <shravanimane33@gmail.com>
2026-07-05 12:30:44 +0530

    Time: 1 ms (97.9%), Space: 42.9 MB (9.8%) - LeetHub

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -1,11 +1,8 @@
 class Solution {
     public int addDigits(int num) {
-        if(num==0){
-            return 0;
-        }
-        else{
-            return 1 + (num -1)%9 ;
-        }
+       if(num==0)
+        return 0;
 
+       return 1+(num-1)%9;
     }
 } 
