3b2bad9bd548638cc1d64a5008187b7e5fa3bda0
Shravani Mane <shravanimane33@gmail.com>
2026-07-13 15:51:59 +0530

    Time: 5 ms (80.57%), Space: 45.8 MB (72.46%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int num = x;
+        int rev = 0;
+        int digit = 0;
+        if(x<0)
+         return false;
+        while(x>0){
+            digit = x%10;
+            rev = rev*10 + digit;
+            x/=10;
+        }
+        if(rev==num){
+            return true;
+        }
+        return false;
+    }
+}
