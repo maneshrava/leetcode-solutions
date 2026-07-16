6afd21e7f820012ad17caad8c3a9cc633e8824ff
Shravani Mane <shravanimane33@gmail.com>
2026-07-16 16:00:02 +0530

    Time: 56 ms (66.5%), Space: 126.3 MB (8%) - LeetHub

--- 3867-sum-of-gcd-of-formed-pairs/3867-sum-of-gcd-of-formed-pairs.java
+++ 3867-sum-of-gcd-of-formed-pairs/3867-sum-of-gcd-of-formed-pairs.java
@@ -1,42 +1,39 @@
 class Solution {
-
-    private int gcd(int a, int b) {
-        while (b != 0) {
-            int t = a % b;
+    private int gcd(int a , int b){
+        while(b!=0){
+            int temp = a%b;
             a = b;
-            b = t;
+            b = temp;
         }
         return a;
     }
 
     public long gcdSum(int[] nums) {
         int n = nums.length;
-
         int[] mx = new int[n];
         mx[0] = nums[0];
 
-        for (int i = 1; i < n; i++) {
-            mx[i] = Math.max(mx[i - 1], nums[i]);
+        for(int i = 1 ; i<n ; i++){
+            mx[i] = Math.max( mx[i-1] , nums[i]);
         }
 
         int[] pre = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            pre[i] = gcd(nums[i], mx[i]);
+        for(int i = 0 ; i<n ; i++){
+            pre[i] = gcd(nums[i],mx[i]);
         }
 
         Arrays.sort(pre);
 
         long sum = 0;
 
-        int l = 0, r = n - 1;
+        int l = 0;
+        int r = n-1;
 
-        while (l < r) {
-            sum += gcd(pre[l], pre[r]);
+        while(l<r){
+            sum+=gcd(pre[r] , pre[l]);
             l++;
             r--;
         }
-
-        return sum;
-    }
+        return sum;    
+    }   
 }
